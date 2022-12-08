1c9412970ba791b30f272d24ce1ba710b66ed4d6
Lily Jiang <ljiang@olin.edu>
2022-12-08 11:25:15 -0500

    add drag and collisions

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,63 +5,97 @@ using UnityEngine;
 public class Car : MonoBehaviour {
     // if true, the car moves horizontally. otherwise, vertical
     public bool horizontal;
+    
+    private bool isDragging = false;
+    private bool stopMoving = false;    // true to stop moving. helps with "collisions"
+    private float offset = 0.0f;
 
-    private bool isDragging;
+
+    void Awake() {
+        if (horizontal) {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+        } else {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
+    }
+
 
     public void OnMouseDown() {
         isDragging = true;
+        if (horizontal) {
+            offset = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+        } else {
+            offset = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+        GetComponent<Collider2D>().isTrigger = true;
     }
+
 
     public void OnMouseUp() {
         // snap to place
         if (horizontal) {
             if (transform.position.x < 0) {
                 Vector2 pos = transform.position;
-                pos.x = -1.739573f;
+                pos.x = -1.5f;
                 transform.position = pos;
             } else {
                 Vector2 pos = transform.position;
-                pos.x = 1.260427f;
+                pos.x = 1.5f;
                 transform.position = pos;
             }
         }
         else {
             if (transform.position.y < 0) {
                 Vector2 pos = transform.position;
-                pos.y = -1.739573f;
+                pos.y = -1.5f;
                 transform.position = pos;
             } else {
                 Vector2 pos = transform.position;
-                pos.y = 1.260427f;
+                pos.y = 1.5f;
                 transform.position = pos;
             }
         }
 
         isDragging = false;
+        GetComponent<Collider2D>().isTrigger = false;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        stopMoving = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        stopMoving = false;
     }
 
     
     void Update() {
-        if (isDragging) {
+        if (isDragging && !stopMoving) {
             if (horizontal) {
-                Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
                 // TODO: change this to not rely on `point` since the mouse can move past it (we want the block to not be able to)
-                if (point.x < -1.739573) {
+                Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                float bounds = 1.55f;
+
+                if (transform.position.x < -bounds && point.x - offset < -bounds) {
                     return;
-                } else if (point.x > 1.260427) {
+                } else if (transform.position.x > bounds && point.x - offset > bounds) {
                     return;
                 }
                 point.y = transform.position.y;
+                point.x = point.x - offset;
                 transform.position = point;
             } else {
                 Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                if (point.y < -1.739573) {
+                float bounds = 1.55f;
+
+                if (transform.position.y < -bounds && point.y - offset < -bounds) {
                     return;
-                } else if (point.y > 1.260427) {
+                } else if (transform.position.y > bounds && point.y - offset > bounds) {
                     return;
                 }
                 point.x = transform.position.x;
+                point.y = point.y - offset;
                 transform.position = point;
             }
         }
